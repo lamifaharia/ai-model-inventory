3b8c200daa69a7db8c91a9aa447856432a421a28
lamifaharia <lamifaharia@gmail.com>
2026-05-24 02:55:31 +0600

    set up project folders

--- client/src/firebase.config.js
+++ client/src/firebase.config.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD1Y047RYO3cGmHGK1kQg6ROC5mIkSbTII",
@@ -7,9 +7,12 @@ const firebaseConfig = {
   projectId: "ai-model-inventory-2e2d7",
   storageBucket: "ai-model-inventory-2e2d7.firebasestorage.app",
   messagingSenderId: "372447216844",
-  appId: "1:372447216844:web:24aef538ff1cf35fd96e69",
-  measurementId: "G-YL4RSD0YYN"
+  appId: "1:372447216844:web:24aef538ff1cf35fd96e69"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+// Export Authentication instances for your client components
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
